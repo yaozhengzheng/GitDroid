fa6405937f4b5cf9b8f8fe0941fd7849dc120891
yao <1624566366@qq>
2016-07-01 09:31:14 +0800

    添加了热门fragment以及界面实现

--- app/src/main/java/com/yao/feicui/gitdroid/hot/pager/RepoListFragment.java
+++ app/src/main/java/com/yao/feicui/gitdroid/hot/pager/RepoListFragment.java
@@ -4,7 +4,6 @@ package com.yao.feicui.gitdroid.hot.pager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.ThemedSpinnerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
