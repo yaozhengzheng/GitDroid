cbe0f1f24f947483a11f997e64e42582c43ac578
yao <1624566366@qq>
2016-07-04 14:19:42 +0800

    添加下拉刷新，上啦加载

--- app/src/main/java/com/yao/feicui/gitdroid/hot/HotRepoPagerAdapter.java
+++ app/src/main/java/com/yao/feicui/gitdroid/hot/HotRepoPagerAdapter.java
@@ -13,24 +13,24 @@ import java.util.List;
 
 /**
  * Created by Administrator on 2016/6/30 0030.
- *
+ * <p/>
  * 此适配器用于HotRepoFragment, 每一页都是一个RepoListFragment.
- *
+ * <p/>
  * 用户浏览过的所有子页面fragment都会保存在内存中，但当它们不可见时，其上的View可能被摧毁。
  * 这可能导致占用大量的内存，因为fragment实例能保存任意量的状态值。
- *
+ * <p/>
  * 在我们的应用内，这是可以接收的，否则应该考虑使用FragmentStatePagerAdapter。
  * Created by 16245 on 2016/06/30.
  */
-public class HotRepoPagerAdapter extends FragmentPagerAdapter{
+public class HotRepoPagerAdapter extends FragmentPagerAdapter {
 
-    private List<String>languages=new ArrayList<>();
+    private List<String> languages = new ArrayList<>();
 
     public HotRepoPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
         //这是测试数据
         for (int i = 0; i < 10; i++) {
-        languages.add("java"+i);
+            languages.add("java" + i);
         }
     }
 
---
+++ app/src/main/java/com/yao/feicui/gitdroid/hot/pager/PtrPageView.java
@@ -0,0 +1,11 @@
+package com.yao.feicui.gitdroid.hot.pager;
+
+import com.hannesdorfmann.mosby.mvp.MvpView;
+
+import java.util.List;
+
+/**
+ * Created by 16245 on 2016/07/04.
+ */
+public interface PtrPageView extends MvpView, PtrView<List<String>>,LoadMoreView<List<String>>{
+}
